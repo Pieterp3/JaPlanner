4a8caa3f7fd6ccfb3a2a516b11407c4cebdfe428
pieterp3 <pbarone96@live.com>
2023-07-04 14:24:39 -0400

    Moved packages. Added Point class. Started shapes

---
+++ ui/panels/components/impl/shapes/Shape.java
@@ -0,0 +1,13 @@
+package ui.panels.components.impl.shapes;
+
+import ui.Frame;
+import ui.panels.components.DrawnComponent;
+
+public abstract class Shape extends DrawnComponent implements Polygon {
+
+    public Shape(Frame frame) {
+        super(frame);
+    }
+    
+
+}
