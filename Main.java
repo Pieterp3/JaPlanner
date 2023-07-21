30f050b6104ba816ea2fe80960e94156ea2977a2
pieterp3 <pbarone96@live.com>
2023-07-21 13:02:20 -0400

    Split engine, Added TTS and Speech Recognition

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import ui.Frame;
 import ui.Panel;
+import util.engine.Engine;
+import util.engine.SpeechEngine;
 import util.structures.List;
 
 public class Main {
@@ -9,15 +11,18 @@ public class Main {
     private static Engine engine;
 
     public static void main(String[] args) {
-        frame = new Frame();
-        engine = new Engine(frame);
-        panels = new List<Panel>();
-        panels.add(new HomePanel(frame));
+       // Speech.speak("Hi baby! What do you want to watch?");
+        
+        
+        // frame = new Frame();
+        engine = new SpeechEngine();
+        // panels = new List<Panel>();
+        // panels.add(new HomePanel(frame));
 
         //Last panel added is the active panel for dev purposes.
-        Panel devPanel = panels.get(DEBUG ? panels.size() - 1 : 0);
-        frame.setActivePanel(devPanel);
-        frame.setVisible(true);
+        // Panel devPanel = panels.get(DEBUG ? panels.size() - 1 : 0);
+        // frame.setActivePanel(devPanel);
+        // frame.setVisible(true);
         engine.start();
     }
 }
